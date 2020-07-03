533ccefdad69fe018ce31c45a7cc9aee6475bce0
Jiue-Ren Liao (Rick) <xrickliao@gmail.com>
2020-07-03 17:07:35 +0800

    update the tf to 2.1 and fix the loss value is 0

--- KWS/MyDNN.py
+++ KWS/MyDNN.py
@@ -7,18 +7,11 @@ import matplotlib.pyplot as plt
 # tf.disable_v2_behavior()
 # tf.reset_default_graph()
 # tf.reset_default_graph()
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
 
-train_data = spio.loadmat('./MyTrainData/kws_train_data_20200703.mat')
-train_lbl = spio.loadmat('./MyTrainData/kws_train_lbl_20200703.mat')
-x_train = train_data['xtrain']
-y_train = train_lbl['ytrain'][0]
-
-
-
-
-# test_data = spio.loadmat('train_data/test.mat')
-# x_test = test_data['x_test']
-# y_test = test_data['y_test']
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def runningMeanFast(x, N):
     return np.convolve(x, np.ones((N,))/N)[(N-1):]
@@ -31,7 +24,7 @@ def datachange(input):
         elif input[i]==2:
             out.append([0,1,0])
         else:
-            out.append([0,0,1])            
+            out.append([0,0,1])
     return out
 
 def next_batch(num, data, labels):
@@ -43,12 +36,21 @@ def next_batch(num, data, labels):
 
     return np.asarray(data_shuffle), np.asarray(labels_shuffle)
 
-    
+train_data = spio.loadmat('./MyTrainData/kws_train_data_20200703.mat')
+train_lbl = spio.loadmat('./MyTrainData/kws_train_lbl_20200703.mat')
+x_train = train_data['xtrain']
+y_train = train_lbl['ytrain'][0]
+y_train = datachange(y_train)
+
+# test_data = spio.loadmat('train_data/test.mat')
+# x_test = test_data['x_test']
+# y_test = test_data['y_test']
+
 # y_train = datachange(y_train)
 # y_test = datachange(y_test)
 
 # Parameters
-learning_rate = 0.01
+learning_rate = 0.0001
 batch_size = 32
 n_band = 1600#40*41 # mfcc dim * context size
 #n_band = 10*41 # filter dim * context size (i)
@@ -61,7 +63,7 @@ optimizer_name = "Adam"
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, n_band])
 # y = tf.placeholder(tf.float32, [None, n_classes])
-y = tf.placeholder(tf.int64, [None])
+y = tf.placeholder('float')
 
 
 weights = {
@@ -77,21 +79,36 @@ biases = {
     'out': tf.Variable(tf.zeros([n_classes],dtype=tf.float32))
 }
 
-##train
-x1 = tf.nn.relu(tf.add(tf.matmul(x, weights['hidden1']), biases['hidden1']))
-# x1=tf.nn.dropout(x1,0.95)
-x2 = tf.nn.relu(tf.add(tf.matmul(x1, weights['hidden2']), biases['hidden2']))
-# x2=tf.nn.dropout(x2,0.95)
-x3 = tf.nn.relu(tf.add(tf.matmul(x2, weights['hidden3']), biases['hidden3']))
-# x2=tf.nn.dropout(x2,0.95)
-logits = tf.matmul(x3, weights['out']) + biases['out']
-loss = tf.reduce_mean(tf.losses.sparse_softmax_cross_entropy(logits=logits, labels=y))
-train_step = tf.contrib.layers.OPTIMIZER_CLS_NAMES[optimizer_name](learning_rate).minimize(loss) # training logic
-float_logit = tf.cast(tf.argmax(logits, 1), tf.float32)
-float_y = tf.cast(y, tf.float32)
-print("logit is {} and y is {}".format(float_logit, float_y))
-correct_prediction = tf.equal(float_logit, float_y)
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) #calculating the accuracy
+##define network
+def dnn128(input_data):
+    weights = {
+        'hidden1': tf.Variable(tf.random_normal([n_band, n_hidden1], dtype=tf.float32, stddev=0.1)),
+        'hidden2': tf.Variable(tf.random_normal([n_hidden1, n_hidden2], dtype=tf.float32, stddev=0.1)),
+        'hidden3': tf.Variable(tf.random_normal([n_hidden2, n_hidden3], dtype=tf.float32, stddev=0.1)),
+        'out': tf.Variable(tf.random_normal([n_hidden3, n_classes], dtype=tf.float32, stddev=0.1))
+    }
+    biases = {
+        'hidden1': tf.Variable(tf.zeros([n_hidden1], dtype=tf.float32)),
+        'hidden2': tf.Variable(tf.zeros([n_hidden2], dtype=tf.float32)),
+        'hidden3': tf.Variable(tf.zeros([n_hidden3], dtype=tf.float32)),
+        'out': tf.Variable(tf.zeros([n_classes], dtype=tf.float32))
+    }
+
+    x1 = tf.nn.relu(tf.add(tf.matmul(input_data, weights['hidden1']), biases['hidden1']))
+    # x1=tf.nn.dropout(x1,0.95)
+    x2 = tf.nn.relu(tf.add(tf.matmul(x1, weights['hidden2']), biases['hidden2']))
+    # x2=tf.nn.dropout(x2,0.95)
+    x3 = tf.nn.relu(tf.add(tf.matmul(x2, weights['hidden3']), biases['hidden3']))
+    # x2=tf.nn.dropout(x2,0.95)
+    logits = tf.matmul(x3, weights['out']) + biases['out']
+    return logits
+
+prediction = dnn128(x)
+# loss = tf.reduce_mean(tf.losses.sparse_softmax_cross_entropy(logits=logits, labels=y))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)  #training logic
+
+
 
 error_train=[]
 error_test=[]
@@ -110,28 +127,39 @@ saver = tf.train.Saver()
 
 with tf.Session() as sess:
     sess.run(init)
-    
+
     '''    Restore variables from disk.    '''
 #    checkpoint = tf.train.latest_checkpoint("checkpoints/checkpoints_1017_256")
 #    saver.restore(sess, checkpoint)
     epochnum=501
+    epoch_loss = []
     for epoch in range(epochnum):
 #        for step in range(60):
         for step in range(100):
             x_n, y_n = next_batch(batch_size, x_train, y_train)
-            sess.run(train_step, feed_dict={x: x_n, y: y_n})
-        loss_value = sess.run(loss, feed_dict={x: x_n, y: y_n})
-        acc = sess.run(accuracy, feed_dict={x: x_n, y: y_n})
+            c, loss_value = sess.run([train_step, loss], feed_dict={x: x_n, y: y_n})
+            epoch_loss.append(loss_value)
+            print('Epoch', epoch, 'completed out of', step, 'loss:', loss_value)
 
-        print('Step {}: rate {}, accuracy {}, Loss {}'.format(epoch, learning_rate, acc * 100, loss_value))
+        # float_logit = tf.cast(tf.argmax(prediction, 1), tf.float32)
+        # float_y = tf.cast(y, tf.float32)
+        correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) #calculating the accuracy
+        acc = accuracy.eval(feed_dict={x: x_train, y: y_train})
+        print('Accuracy:', acc)
+        # print('Step {}: rate {}, accuracy {}, Loss {}'.format(epoch, learning_rate, acc * 100, loss_value))
+        plt.subplot(1, 2, 1)
+        plt.plot(loss_value)
+        plt.title('Epoch Loss')
+        plt.show()
         # acc1 = sess.run(accuracy, feed_dict={x: x_test, y: y_test})
             # spio.savemat('weight/w_3layer128.mat', {'w1': weight1,'w2':weight2,'w3':weight3,'w4':weight4,
             #                                         'b1':bias1,'b2':bias2,'b3':bias3,'b4':bias4})
 
     '''    # Save the variables to disk. '''
     #save_path = saver.save(sess, "checkpoints/checkpoints_1211/model_1211d_ep_{}.ckpt".format(epoch))
-   
-        
+
+
 print("training finished!")
 # plt.plot(range(len(error_train)), error_train, 'b', label='Training accuracy')
 # plt.plot(range(len(error_test)), error_test, 'r', label='Test accuracy')
